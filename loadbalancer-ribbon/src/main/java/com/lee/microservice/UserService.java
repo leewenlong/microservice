0e68b2a7881daee45e8779b7886db638296cf834
liwenlong <liwenlong@oneapm.com>
2017-05-23 18:19:50 +0800

    add hystrix

--- loadbalancer-ribbon/src/main/java/com/lee/microservice/UserService.java
+++ loadbalancer-ribbon/src/main/java/com/lee/microservice/UserService.java
@@ -1,10 +1,12 @@
 package com.lee.microservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author LiWenLong
@@ -12,17 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2017/5/23
  * @Copyright (C) 2008-2017 oneapm.com. all rights reserved.
  */
-@RestController
-public class UserController {
+@Service
+public class UserService {
     @Autowired
-    UserService service;
+    RestTemplate restTemplate;
 
 
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "error")
     public String user(@PathVariable String id) {
-        return service.user(id);
+        return restTemplate.getForEntity("http://provider-user/user/"+id, String.class).getBody();
     }
 
-
+    public String error(String id){
+        return "error========="+id;
+    }
 
 }
